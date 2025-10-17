59b918c30942bd44095f228f9d87711d5fe40d6b
AKS <37595327+AmirK-S@users.noreply.github.com>
2025-10-17 22:00:44 +0000

    basic setup

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/healthz")
+def health():
+    return {"ok": True}
